155bbf2aae9235b25643b32860e8d85de3c98b8f
Mason Mackaman <masondeanm@aol.com>
2021-05-18 20:43:55 -0400

    Add documentation

--- src/RefEq.purs
+++ src/RefEq.purs
@@ -2,8 +2,10 @@ module RefEq where
 
 import Prelude
 
+-- | The purpose of this data type is to give an `Eq` instance to anything via referential equality.
 data RefEq a = RefEq a
 
+-- | Referential equality
 foreign import refEq :: ∀ a b. a -> b -> Boolean
 
 infix 4 refEq as ===
