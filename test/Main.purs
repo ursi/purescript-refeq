813d874565882e8e4ed9256b9a6033a094f9d9b3
Mason Mackaman <masondeanm@aol.com>
2021-05-19 13:35:19 -0400

    Add more tests

--- test/Main.purs
+++ test/Main.purs
@@ -1,7 +1,7 @@
 module Test.Main where
 
 import MasonPrelude
-import Test.Assert (assert)
+import Test.Assert (assert, assertFalse)
 
 import RefEq (RefEq(..), (===))
 
@@ -9,7 +9,12 @@ import RefEq (RefEq(..), (===))
 f :: ∀ a. a -> a
 f = identity
 
+g :: ∀ a. a -> a
+g = \a -> a
+
 main :: Effect Unit
 main = do
   assert $ f === f
   assert $ RefEq f == RefEq f
+  assertFalse $ f === g
+  assertFalse $ RefEq f == RefEq g
